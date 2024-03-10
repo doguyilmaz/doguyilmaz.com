a6d5396475f18f6dad7e1a3aa7ff0a35fb3ec008
Dogu K. Yilmaz <dogukyilmaz@gmail.com>
2024-03-10 23:17:21 +0300

    colors

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,1 +1,42 @@
 export const SITE_URL = 'https://doguyilmaz.com';
+
+const LANG_COLORS = {
+  js: {
+    bg: '#f7df1e',
+    text: 'JavaScript',
+    color: 'black',
+  },
+  ts: {
+    bg: '#007acc',
+    text: 'TypeScript',
+    color: 'white',
+  },
+  react: {
+    bg: '#61dafb',
+    text: 'React',
+    color: 'black',
+  },
+  vue: {
+    bg: '#41b883',
+    text: 'Vue',
+    color: 'black',
+  },
+  rn: {
+    bg: '#61dafb',
+    text: 'React Native',
+    color: 'black',
+  },
+  swift: {
+    bg: '#ffac45',
+    text: 'Swift',
+    color: 'white',
+  },
+  astro: {
+    bg: '#fff',
+    text: 'Astro',
+    color: 'white',
+  },
+};
+
+export const getExperience = () => new Date().getFullYear() - 2019;
+export const getLangColor = (lang: keyof typeof LANG_COLORS) => `<span style=" color: ${LANG_COLORS[lang].bg}; ">${LANG_COLORS[lang].text}</span>`;
