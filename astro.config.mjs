5a8968d9d28070296478bb29a19ba35fdc7b0feb
Dogu K. Yilmaz <dogukyilmaz@gmail.com>
2024-03-11 02:52:15 +0300

    vercel stats

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import robotsTxt from 'astro-robots-txt';
+import vercel from '@astrojs/vercel/serverless';
 import { SITE_URL } from './src/data/config';
 
 // https://astro.build/config
@@ -15,4 +16,8 @@ export default defineConfig({
       wrap: false,
     },
   },
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
 });
