1f6153ba93a5b486081ad0f86a86e40ff9791f53
Dogu Yilmaz <dogukyilmaz@gmail.com>
2024-12-31 02:50:05 +0100

    adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,12 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
 import mdx from "@astrojs/mdx";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://doguyilmaz.com",
   output: "server",
+  adapter: vercel(),
   integrations: [tailwind(), mdx(), sitemap(), prefetch({ throttle: 3 })],
 });
